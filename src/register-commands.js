3d56e5dd2ee89d894f4999e451b06f8fa5266705
Dominic <dompaulazzo@gmail.com>
2023-05-17 11:27:38 -0700

    added slash command option and the bot can now add the sum of two numbers

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,10 +1,53 @@
 require('dotenv').config();
-const { REST, Routes} = require('discord.js');
+
+const { REST, Routes, ApplicationCommandOptionType} = require('discord.js');
 
 const commands = [
     {
-        name: 'hey',
-        description: 'HELLO!',
+      name: 'add',
+      description: 'Adds two numbers.',
+      options: [
+        {
+            name: 'first-number',
+            description: 'The first number.',
+            type: ApplicationCommandOptionType.Number,
+            choices: [
+                {
+                    name: 'one',
+                    value: 1,
+                },
+                {
+                    name: 'two',
+                    value: 2,
+                },
+                {
+                    name: 'three',
+                    value: 3,
+                },
+            ],
+            required: true,
+        },
+        {
+            name: 'second-number',
+            description: 'The second number.',
+            type: ApplicationCommandOptionType.Number,
+            choices: [
+                {
+                    name: 'one',
+                    value: 1,
+                },
+                {
+                    name: 'two',
+                    value: 2,
+                },
+                {
+                    name: 'three',
+                    value: 3,
+                },
+            ],
+            required: true,
+        },
+      ]  
     }
 ];
 
